93e90ae32f0a6c0321b6ef192a091250ddd83889
Andrew Gerashchenko <andgerashchenko@gmail.com>
2022-07-16 14:49:23 -0400

    trigger practice

--- src/test/java/cydeo/Driver.java
+++ src/test/java/cydeo/Driver.java
@@ -22,6 +22,7 @@ public class Driver {
     }
     private static WebDriver driver;
 
+
     public static WebDriver getDriver() {
         if (driver == null) {
             if (System.getProperty("BROWSER") == null) {
